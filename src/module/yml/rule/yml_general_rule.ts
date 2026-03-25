c7691b9b0532b02b9517258cc7a645b55722b95f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:45:22 -0400

    refactor: simplify ymlGeneralRule structure and improve formatting for consistency

--- src/module/yml/rule/yml_general_rule.ts
+++ src/module/yml/rule/yml_general_rule.ts
@@ -1,55 +1,35 @@
+import { INDENT } from '@global/constant/format_rules';
 import { stylisticGeneralRule } from '@module/stylistic/rule/stylistic_general_rule';
 
 import type { Linter } from 'eslint';
 
-const indent = 2;
-
-const ymlGeneralRule: Linter.RulesRecord =
-{
+const ymlGeneralRule: Linter.RulesRecord = {
   'yml/block-mapping': 'error',
   'yml/block-mapping-colon-indicator-newline': 'error',
   'yml/block-mapping-question-indicator-newline': 'error',
   'yml/block-sequence': 'error',
   'yml/block-sequence-hyphen-indicator-newline': 'error',
-  'yml/file-extension':
-  [
-    'error',
-    {
-      caseSensitive: true,
-      extension: 'yml'
-    }
-  ],
+  'yml/file-extension': ['error', { caseSensitive: true, extension: 'yml' }],
   'yml/flow-mapping-curly-newline': 'error',
   'yml/flow-mapping-curly-spacing': stylisticGeneralRule['style/object-curly-spacing'] ?? 'off',
   'yml/flow-sequence-bracket-newline': stylisticGeneralRule['style/array-bracket-newline'] ?? 'off',
   'yml/flow-sequence-bracket-spacing': stylisticGeneralRule['style/array-bracket-spacing'] ?? 'off',
-  'yml/indent': [ 'error', indent ],
-  'yml/key-spacing':
-  [
-    'error',
-    {
-      afterColon: true,
-      beforeColon: false,
-      mode: 'strict'
-    }
-  ],
+  'yml/indent': ['error', INDENT],
+  'yml/key-spacing': ['error', {
+    afterColon: true,
+    beforeColon: false,
+    mode: 'strict'
+  }],
   'yml/no-empty-document': 'error',
   'yml/no-empty-key': 'error',
   'yml/no-empty-mapping-value': 'error',
   'yml/no-empty-sequence-entry': 'error',
   'yml/no-irregular-whitespace': 'error',
-  'yml/no-multiple-empty-lines': [ 'error', { max: 1 } ],
+  'yml/no-multiple-empty-lines': ['error', { max: 1 }],
   'yml/no-tab-indent': 'error',
   'yml/no-trailing-zeros': 'error',
   'yml/plain-scalar': 'error',
-  'yml/quotes':
-  [
-    'error',
-    {
-      avoidEscape: false,
-      prefer: 'single'
-    }
-  ],
+  'yml/quotes': ['error', { avoidEscape: false, prefer: 'single' }],
   'yml/require-string-key': 'error',
   'yml/spaced-comment': 'error',
   'yml/vue-custom-block/no-parsing-error': 'error'
