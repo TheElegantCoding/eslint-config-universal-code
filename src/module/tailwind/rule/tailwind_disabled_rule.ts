7eb8607decef0fd795e459e11572f3a84dac09a6
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:44:12 -0400

    refactor: remove tailwindDisabledRule for improved configuration clarity

--- src/module/tailwind/rule/tailwind_disabled_rule.ts
+++
@@ -1,9 +0,0 @@
-import type { Linter } from 'eslint';
-
-const tailwindDisabledRule: Linter.RulesRecord =
-{
-  'tailwind/no-arbitrary-value': 'off',
-  'tailwind/no-custom-classname': 'off'
-};
-
-export { tailwindDisabledRule };
