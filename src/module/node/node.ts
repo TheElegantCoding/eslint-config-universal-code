1f5fea0c22b6ace3857460639462917400690139
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:43:14 -0400

    refactor: streamline node configuration and remove unused rules

--- src/module/node/node.ts
+++ src/module/node/node.ts
@@ -1,22 +1,15 @@
-import { nodeDisabledRule } from '@module/node/rule/node_disabled_rule';
-import { nodeGeneralRule } from '@module/node/rule/node_general_rule';
 import pluginNode from 'eslint-plugin-n';
 
 import type { Linter } from 'eslint';
+import { PACKAGE_NAME } from '@global/constant/package_name';
 
-const node: Linter.Config =
-{
-  name: 'node',
-  plugins:
-  {
-    node: pluginNode
-  },
-  rules:
-  {
-    ...nodeGeneralRule,
-    ...nodeDisabledRule
+export const node: Linter.Config = {
+  name: `${PACKAGE_NAME}/node`,
+  plugins: { node: pluginNode },
+  rules: {
+    'node/no-deprecated-api': 'error',
+    'node/no-path-concat': 'error',
+    'node/prefer-node-protocol': 'error',
+    'node/process-exit-as-throw': 'error'
   }
-
 };
-
-export { node };
