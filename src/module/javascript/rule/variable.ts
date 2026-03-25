ca5dcb49c738a231a532ebcc46ab1c526cc3f031
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-24 23:42:49 -0400

    refactor: improve formatting of variable rules for consistency

--- src/module/javascript/rule/variable.ts
+++ src/module/javascript/rule/variable.ts
@@ -1,10 +1,9 @@
 import type { Linter } from 'eslint';
 
-const variable: Linter.RulesRecord =
-{
+const variable: Linter.RulesRecord = {
   'block-scoped-var': 'error',
-  'one-var': [ 'error', 'never' ],
-  'require-atomic-updates': [ 'error', { allowProperties: true } ],
+  'one-var': ['error', 'never'],
+  'require-atomic-updates': ['error', { allowProperties: true }],
   'vars-on-top': 'error'
 };
 
