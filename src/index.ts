676ffa489f62fe6da7e3e8b0b4818828641a8d18
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-25 15:14:14 -0400

    refactor: reorder imports and enable unicorn and perfectionist options in ESLint configuration

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,20 @@
+/* eslint-disable complexity */
 /* eslint-disable max-statements */
 import { fileIgnore } from '@global/ignores';
-
-import type { ConfigurationOption } from '@global/type/configuration_option';
 import { astro } from '@module/astro/astro';
 import { html } from '@module/html/html';
 import { imports } from '@module/import/import';
 import { javascript } from '@module/javascript/javascript';
 import { json } from '@module/json/json';
 import { node } from '@module/node/node';
+import { perfectionist } from '@module/perfectionist/perfectionist';
 import { regex } from '@module/regex/regex';
 import { stylistic } from '@module/stylistic/stylistic';
 import { typescript } from '@module/typescript/typescript';
+import { unicorn } from '@module/unicorn/unicorn';
 import { yml } from '@module/yml/yml';
+
+import type { ConfigurationOption } from '@global/type/configuration_option';
 import type { Linter } from 'eslint';
 
 const eslintSetup = (option: ConfigurationOption, override: Linter.Config | Linter.Config[] = []): Linter.Config[] => {
@@ -22,13 +25,11 @@ const eslintSetup = (option: ConfigurationOption, override: Linter.Config | Lint
     javascript,
     node,
     imports
-
-    /* unicorn,
-       perfectionist */
-
   );
 
   if (option.stylistic ?? false) { config.push(stylistic); }
+  if (option.unicorn ?? false) { config.push(unicorn); }
+  if (option.perfectionist ?? false) { config.push(perfectionist); }
   if (option.typescript ?? false) { config.push(...typescript); }
   if (option.html ?? false) { config.push(html); }
   if (option.astro ?? false) { config.push(...astro); }
