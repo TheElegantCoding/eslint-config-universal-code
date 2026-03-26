cbe03c3f4d631b81984f74d0b955617cb58e6d16
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-25 21:36:01 -0400

    refactor: add unicorn and perfectionist options to ConfigurationOption type

--- src/global/type/configuration_option.ts
+++ src/global/type/configuration_option.ts
@@ -6,9 +6,11 @@ type ConfigurationOption = {
   readonly solid?: boolean;
   readonly astro?: boolean;
   readonly ignore?: string[];
+  readonly unicorn?: boolean;
   readonly tailwind?: boolean;
   readonly stylistic?: boolean;
   readonly typescript?: boolean;
+  readonly perfectionist?: boolean;
 };
 
 export type { ConfigurationOption };
