9a1901c0dd1b020bb86071f7cfce252c75651f49
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-25 21:36:30 -0400

    refactor: remove duplicate import of PACKAGE_NAME in javascript.ts

--- src/module/javascript/javascript.ts
+++ src/module/javascript/javascript.ts
@@ -1,3 +1,4 @@
+import { PACKAGE_NAME } from '@global/constant/package_name';
 import { bestPractice } from '@module/javascript/rule/best_practice';
 import { error } from '@module/javascript/rule/error';
 import { strict } from '@module/javascript/rule/strict';
@@ -6,7 +7,6 @@ import { variable } from '@module/javascript/rule/variable';
 import globals from 'globals';
 
 import type { Linter } from 'eslint';
-import { PACKAGE_NAME } from '@global/constant/package_name';
 
 const javascript: Linter.Config = {
   name: `${PACKAGE_NAME}/javascript`,
