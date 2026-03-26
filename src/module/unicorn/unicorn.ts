0392db7a5c20f8a7f5eadb08cda50caed3d6355f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-03-25 21:37:14 -0400

    refactor: update unicorn config name to include PACKAGE_NAME

--- src/module/unicorn/unicorn.ts
+++ src/module/unicorn/unicorn.ts
@@ -1,10 +1,11 @@
+import { PACKAGE_NAME } from '@global/constant/package_name';
 import { unicornGeneralRule } from '@module/unicorn/rule/unicorn_general_rule';
 import pluginUnicorn from 'eslint-plugin-unicorn';
 
 import type { Linter } from 'eslint';
 
 const unicorn: Linter.Config = {
-  name: 'unicorn',
+  name: `${PACKAGE_NAME}/unicorn`,
   plugins: { unicorn: pluginUnicorn },
   rules: unicornGeneralRule
 };
