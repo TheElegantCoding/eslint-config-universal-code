d81076f6489d9462b084cbc4d5773f7274fe8d9f
TheElegantCoding <luismonsalve1941@gmail.com>
2026-04-02 12:57:02 -0400

    fix: remove 'import/group-exports' rule from import configuration

--- src/module/import/import.ts
+++ src/module/import/import.ts
@@ -7,7 +7,6 @@ const imports: Linter.Config[] = [{
   name: `${PACKAGE_NAME}/import`,
   plugins: { import: pluginImport as unknown as Plugin },
   rules: {
-    'import/group-exports': 'error',
     'import/no-namespace': 'error',
     'import/no-mutable-exports': 'error',
     'import/no-amd': 'error',
